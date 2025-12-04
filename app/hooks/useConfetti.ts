11eb2caab3d468ec8dc0ca99e8cf364f42cdefe1
Esha Wadher <eshaaw@bu.edu>
2025-12-03 21:36:01 -0500

    add customization

--- app/hooks/useConfetti.ts
+++ app/hooks/useConfetti.ts
@@ -1,5 +1,9 @@
 'use client';
 
+//made a custom react hook
+//customize location
+//customic number of confetti
+//customize how much confetti
 import confetti from 'canvas-confetti';
 
 export function useConfetti() {
@@ -7,7 +11,10 @@ export function useConfetti() {
         confetti({
             particleCount: 1000,
             spread: 90,
-            origin: { y: 0.6 },
+            origin: { y: 0.7 },
+            //colors: ["#FF6B6B", "#FFD93D", "#6BCB77", "#4D96FF"],
+            colors: ["#9CC6DB", "#050E3C", "#434E78", "#3291B6"],
+            //colors: ["#DC143C", "#E43636", "#CD2C58", "#FCC6FF"],
         });
     };
 
